c6d38894c403a529b1f304705fcbd2c4922c9250
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2023-01-27 13:25:02 +0300

    refactoring: change to function component

--- src/compontents/Square/Square.tsx
+++ src/compontents/Square/Square.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './square.module.css';
 
 export type SquareValue = 'X' | 'O' | null;
@@ -8,23 +8,10 @@ interface ISquareProps {
   onClick: () => void;
 }
 
-interface ISquareState {
-  value: SquareValue;
-}
-
-export class Square extends React.Component<ISquareProps, ISquareState> {
-  constructor(props: ISquareProps) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+export function Square({ value, onClick }: ISquareProps) {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 }
